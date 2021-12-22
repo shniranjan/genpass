12ba49ab3d02a35d5e6990e4dbca3d1765b0e79a
Niranjan Shrestha <shniranjan@gmail.com>
2021-12-23 00:43:40 +1030

    setup modified for required package check and readme file changed

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 #  genpass
 
+The program is based on Debian, any distros based on Debian should work.
+
+Install dependent package prior to installation.
+Dependent package: bc
+
 This program generate random alpha-numerial texts including $ and # sign to be used as password or secret code. <br>
 
 To install this program:<br>
--- setup.sh
+++ setup.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+#check dependent package installed or not
+if [ -z $(which bc) ]; then
+	echo 'Required package (bc) not installed.  Aboarting installation...'
+	sleep 2
+	exit 1
+
 #check if genpass exists, if not download
 [ ! -f genpass ] && wget https://raw.githubusercontent.com/shniranjan/genpass/main/genpass
 
+#enable executable
+chmod +x genpass
 #copy genpass into bin directory
 sudo cp ./genpass /usr/local/bin
 
+exit 0
