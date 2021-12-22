d4910553d56aab48980fb16ba9ead4b39e214b86
Niranjan Shrestha <shniranjan@gmail.com>
2021-12-23 00:12:30 +1030

    setup file modified

--- setup.sh
+++ setup.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+
+#check if genpass exists, if not download
+[ ! -f genpass ] && wget https://raw.githubusercontent.com/shniranjan/genpass/main/genpass
+
+#copy genpass into bin directory
 sudo cp ./genpass /usr/local/bin
+
